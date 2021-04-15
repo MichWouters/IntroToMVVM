d790317f2bd04f96d78bbf973f9ec6a03c724ef9
Michiel Wouters <michiel_woute64@hotmail.com>
2021-04-15 09:41:25 +0200

    Add project files.

---
+++ IntroToMVVM/IntroToMVVM/Models/ToDoItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntroToMVVM.Models
+{
+    public class ToDoItem
+    {
+        public int ID { get; set; }
+
+        public string Text { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        public ToDoItem(string text, bool isCompleted)
+        {
+            Text = text;
+            IsCompleted = isCompleted;
+        }
+    }
+}
---
+++ IntroToMVVM/IntroToMVVM/ViewModels/ToDoListViewModel.cs
@@ -0,0 +1,41 @@
+﻿using IntroToMVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace IntroToMVVM.ViewModels
+{
+    public class ToDoListViewModel
+    {
+        public ObservableCollection<ToDoItem> ToDoItems { get; set; }
+
+        public string NewToDoText { get; set; }
+
+        public Command AddToDoCommand => new Command(AddItem);
+        public Command DeleteCommand => new Command(RemoveItem);
+
+        public ToDoListViewModel()
+        {
+            ToDoItems = new ObservableCollection<ToDoItem>()
+            {
+               new ToDoItem("Todo 1", false),
+               new ToDoItem("Todo 2", false),
+               new ToDoItem("Todo 3", false),
+            };
+        }
+
+        private void AddItem()
+        {
+            var todo = new ToDoItem(NewToDoText, false);
+            ToDoItems.Add(todo);
+        }
+
+        private void RemoveItem(object obj)
+        {
+            var todo = obj as ToDoItem;
+            ToDoItems.Remove(todo);
+        }
+    }
+}
